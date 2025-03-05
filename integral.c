b57e8f26c9d8d7acc5d9b9a06040d5796ada34e1
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2025-03-05 20:20:57 +0600

    fix

--- integral.c
+++ integral.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <math.h>
 #include "integral.h"
+#include <math.h>
+#include <stdio.h>
 
-double integral(double a,double b,double (*f)(double))
+double integral(double a, double b, double (*f)(double))
 {
-    double step = (b - a)/1000;
+    double step = (b - a) / 1000;
 
     double sum = 0;
 
     for (int i = 0; i < 1000; i++)
     {
-        sum += 0.5*step*(f(a + step*i)+f(a + step*(i+1)));
+        sum += 0.5 * step * (f(a + step * i) + f(a + step * (i + 1)));
     }
     return sum;
 }
--- integral_test.c
+++ integral_test.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <math.h>
-#include <assert.h>
 #include "integral.h"
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
 #define EXP 2.7182818284590452354
 
 double cosx(double x)
@@ -11,39 +11,42 @@ double cosx(double x)
 
 double parabola(double x)
 {
-    return x*x;
+    return x * x;
 }
 
 double expx(double x)
 {
-    return pow(EXP,x);
+    return pow(EXP, x);
 }
 
-void test_cosx() {
-  double a = 0;
-  double b = 1;
+void test_cosx()
+{
+    double a = 0;
+    double b = 1;
 
-  double res = integral(a,b,cosx);
+    double res = integral(a, b, cosx);
 
-  assert(fabs(res - 0.84147 ) < 0.00001);
+    assert(fabs(res - 0.84147) < 0.00001);
 }
 
-void test_parabola() {
-  double a = 0;
-  double b = 1;
+void test_parabola()
+{
+    double a = 0;
+    double b = 1;
 
-  double res = integral(a,b,parabola);
+    double res = integral(a, b, parabola);
 
-  assert(fabs(res - 0.33333 ) < 0.00001);
+    assert(fabs(res - 0.33333) < 0.00001);
 }
 
-void test_expx() {
-  double a = 0;
-  double b = 1;
+void test_expx()
+{
+    double a = 0;
+    double b = 1;
 
-  double res = integral(a,b,expx);
+    double res = integral(a, b, expx);
 
-  assert(fabs(res - 1.71828 ) < 0.00001);
+    assert(fabs(res - 1.71828) < 0.00001);
 }
 
 int main()
